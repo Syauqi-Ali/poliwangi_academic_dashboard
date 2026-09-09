d5ee7b98bb75ffe6603b59430dda0fdc5e18dfb6
SyauqiAli <sauqi2019@gmail.com>
2026-09-09 10:04:09 +0700

    eat(w03): implement gorouter navigation and riverpod state management for krs app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'package:poliwangi_academic_dashboard/modul_01/profile_screen.dart';
+import 'package:poliwangi_academic_dashboard/modul_02/academic_dashboard_screen.dart';
 import 'package:poliwangi_academic_dashboard/modul_03/modul_03_app.dart';
 
 void main(List<String> args) {
-  runApp(Modul03App());
+  runApp(AcademicDashboardScreen());
 }
