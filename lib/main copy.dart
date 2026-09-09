9d2cb502bc36bc264a7a9300419177d9bf2f2eeb
SyauqiAli <sauqi2019@gmail.com>
2026-09-09 09:52:13 +0700

    first commit

---
+++ lib/main copy.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      title: "Dashboard Akademik TRPL",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF0284C7),
+          brightness: Brightness.light,
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const new({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("RPL2A Jaya!")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [Text("Halo")],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:poliwangi_academic_dashboard/modul_01/profile_screen.dart';
+import 'package:poliwangi_academic_dashboard/modul_03/modul_03_app.dart';
+
+void main(List<String> args) {
+  runApp(Modul03App());
+}
